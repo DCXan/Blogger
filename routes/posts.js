cd375f1490906d9037af74f294a9aea9523a9394
DCXan <chen.david.1016@gmail.com>
2022-06-22 00:05:45 -0500

    update post function added

--- routes/posts.js
+++ routes/posts.js
@@ -15,13 +15,12 @@ postRouter.get('/', (req, res) => {
 
 postRouter.post('/create', (req, res) => {
 
-    console.log(req.body)
     const title = req.body.title
     const post = req.body.body
 
-    db.none(`INSERT INTO posts (title, body) VALUES ('${title}', '${post}')`)
+    // db.none(`INSERT INTO posts (title, body) VALUES ('${title}', '${post}')`)
 
-    // db.none('INSERT INTO posts (title, body) VALUES ($1, $2)', [title, post])
+    db.none('INSERT INTO posts (title, body) VALUES ($1, $2)', [title, post])
     .then(() => {
         res.redirect('/posts')
     }).catch(error => {
@@ -34,11 +33,34 @@ postRouter.get('/create', (req, res) => {
 })
 
 postRouter.post('/delete', (req, res) => {
+
     const id = req.body.postID
     
-    db.none(`DELETE FROM posts WHERE post_id = ${id}`)
+    db.none('DELETE FROM posts WHERE post_id = ($1)', [id])
+
+    // db.none(`DELETE FROM posts WHERE post_id = ${id}`)
+
+    res.redirect('/posts')
+})
+
+postRouter.post('/update/:postID', (req, res) => {
+    const id = req.params.postID
+
+    db.one('SELECT post_id, title, body, date_created, date_updated, is_published FROM posts WHERE post_id = ($1)', [id])
+    .then(post => {
+        res.render('update-post', {post})
+    })
+})
+
+postRouter.post('/update', (req, res) => {
+    const id = parseInt(req.body.postID)
+    const title = req.body.title
+    const post = req.body.body
+
+    db.none('UPDATE posts SET title = ($1), body = ($2), date_updated = current_timestamp WHERE post_id = ($3)', [title, post, id])
     
     res.redirect('/posts')
+    
 })
 
 module.exports = postRouter
